4baa0ca9b42edcb6f8812ba008009e6fd030d552
Walid Naiji <wnaiji@gmail.com>
2023-09-26 00:13:11 +0200

    parser

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,13 +6,15 @@
 /*   By: walidnaiji <walidnaiji@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/15 10:56:57 by wnaiji            #+#    #+#             */
-/*   Updated: 2023/09/25 17:45:22 by walidnaiji       ###   ########.fr       */
+/*   Updated: 2023/09/26 00:11:31 by walidnaiji       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parser.h"
 /*
--faire en sorte que "cat -e" soit dans le même maillon
+-pour pouvoir différencier un SPACE de " " il faudra split les args cmds
+	dés le début et non par la suite car une fois les arguments concaténé
+	on ne poura plus faire la différence entre un SPACE et " "
 -split les cmds puis vérifier le premier str du tableau via strlower
 	si c'est un builtin
 -une fois que la liste et construite, que la vérification des enum
@@ -79,20 +81,13 @@ t_parser	*is_cmd_or_builtin(t_lexer **lexer, t_parser *parser)
 		if ((*lexer)->operator == NO_OPERATOR)
 		{
 			parser = parser_add_back_list(parser, NULL);
-			init_node_cmd(&parser, &*lexer);
-			if (*lexer)
+			while (*lexer && (*lexer)->operator == NO_OPERATOR)
 			{
-				while (*lexer && (*lexer)->operator == NO_OPERATOR)
-				{
+				init_node_cmd(&parser, &*lexer);
+				if (*lexer && (*lexer)->operator == SPACE
+					&& (*lexer)->next->operator == NO_OPERATOR)
 					init_node_cmd(&parser, &*lexer);
-					if (*lexer && (*lexer)->operator == SPACE
-						&& (*lexer)->next->operator == NO_OPERATOR)
-						init_node_cmd(&parser, &*lexer);
-					else if (*lexer)
-						*lexer = (*lexer)->next;
-				}
 			}
-
 		}
 		else
 			*lexer = (*lexer)->next;
@@ -116,5 +111,6 @@ void	init_parser(t_lexer *lexer)
 	parser = is_cmd_or_builtin(&lexer, parser);
 	lexer = first_node;
 	//parser = init_node_parser(parser);
+	print_lexer(lexer);
 	print_parser(parser);
 }
